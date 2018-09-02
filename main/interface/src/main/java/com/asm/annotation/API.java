358465650d41fe5e930ddd5ec7bd82088a519060
LHW <lhwdev6@gmail.com>
2018-09-02 09:17:10 +0900

    Added some documents to @API

--- main/interface/src/main/java/com/asm/annotation/API.java
+++ main/interface/src/main/java/com/asm/annotation/API.java
@@ -1,4 +1,19 @@
 package com.asm.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+
+/**
+ * Shows that class is included in API.
+ * This API is part of ASM, and can access by application or plugin.
+ */
+@Documented
+@Retention(CLASS)
+@Target(TYPE)
 public @interface API
 {}
